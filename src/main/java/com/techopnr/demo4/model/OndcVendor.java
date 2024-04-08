24d557a3b11b5446d34e5b2b2f497af91d00f6b2
Anand <akanand104@gmail.com>
2024-04-09 02:31:19 +0530

    first commit

---
+++ src/main/java/com/techopnr/demo4/model/OndcVendor.java
@@ -0,0 +1,88 @@
+package com.techopnr.demo4.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vendors")
+public class OndcVendor {
+    // modal class with properties/attributes
+
+    @Id
+    private String vendorId;
+    private String vendorName;
+    private String vendorPhoneNumber;
+    private String vendorEmail;
+    private String vendorAddress;
+    private String vendorTaxId;
+
+    
+    // empty constructor
+    public OndcVendor() {
+
+    }
+
+    // constructor with argument in place
+
+    public OndcVendor(String vendorId, String vendorName, String vendorPhoneNumber, String vendorEmail,
+            String vendorAddress, String vendorTaxId) {
+        this.vendorId = vendorId;
+        this.vendorName = vendorName;
+        this.vendorPhoneNumber = vendorPhoneNumber;
+        this.vendorEmail = vendorEmail;
+        this.vendorAddress = vendorAddress;
+        this.vendorTaxId = vendorTaxId;
+    }
+
+
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorPhoneNumber() {
+        return vendorPhoneNumber;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public String getVendorEmail() {
+        return vendorEmail;
+    }
+
+    public void setVendorEmail(String vendorEmail) {
+        this.vendorEmail = vendorEmail;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+    public String getVendorTaxId() {
+        return vendorTaxId;
+    }
+
+    public void setVendorTaxId(String vendorTaxId) {
+        this.vendorTaxId = vendorTaxId;
+    }
+
+}
---
+++ src/main/java/com/techopnr/demo4/service/impl/OndcVendorServiceImpl.java
@@ -0,0 +1,58 @@
+package com.techopnr.demo4.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.techopnr.demo4.model.OndcVendor;
+import com.techopnr.demo4.repository.OndcVendorRepository;
+import com.techopnr.demo4.service.OndcVendorService;
+
+
+
+// Unsatisfied dependency expressed through constructor parameter 0: Error creating bean with name 'ondcVendorServiceImpl' defined in file [D:\Anand\java 2024\demo-4\demo-4\target\classes\com\techopnr\demo4\service\impl\OndcVendorServiceImpl.class]:
+@Service
+public class OndcVendorServiceImpl implements OndcVendorService {
+
+
+  
+// repository  anotation 
+    OndcVendorRepository ondcVendorRepository;
+
+    public OndcVendorServiceImpl(OndcVendorRepository ondcVendorRepository) {
+        this.ondcVendorRepository = ondcVendorRepository;
+    }  // constructor based on  cloudVendorRepository 
+
+
+    @Override
+    public String createOndcVendor(OndcVendor ondcVendor) {
+        ondcVendorRepository.save(ondcVendor);
+        return "created successfully";
+    }
+
+
+    @Override
+    public List<OndcVendor> getAllOndcVendor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public OndcVendor getOndcVendor(String ondcVendorId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public String updateOndcVendor(OndcVendor ondcVendor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+
+
+    
+}
